caa9aa20a9cd08f3eb769406fc2bf2cb59d1c240
Saivaraprasad48 <sai44prasad88@gmail.com>
2024-02-20 15:15:07 +0530

    initial frontend added

---
+++ frontend/src/components/Balance.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/prop-types */
+export const Balance = ({ value }) => {
+  return (
+    <div className="flex">
+      <div className="font-bold text-lg">Your balance</div>
+      <div className="font-semibold ml-4 text-lg">Rs {value}</div>
+    </div>
+  );
+};
