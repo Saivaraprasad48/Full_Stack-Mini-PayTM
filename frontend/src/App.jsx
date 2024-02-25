56ef42989d5af724cbcc8518088b0d7972765b1f
Saivaraprasad48 <sai44prasad88@gmail.com>
2024-02-25 17:18:59 +0530

    updated protected routes for routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
 import { Dashboard } from "./pages/Dashboard";
 import { SendMoney } from "./pages/SendMoney";
 import UserProfile from "./pages/UserProfile";
+import PropTypes from "prop-types";
+
+const isAuthenticated = () => {
+  return localStorage.getItem("token") !== null;
+};
+const ProtectedRoute = ({ element: Element, ...rest }) => {
+  return isAuthenticated() ? <Element {...rest} /> : <Navigate to="/signin" />;
+};
+
+ProtectedRoute.propTypes = {
+  element: PropTypes.element.isRequired,
+};
 
 function App() {
   return (
@@ -12,9 +24,18 @@ function App() {
         <Routes>
           <Route path="/signup" element={<Signup />} />
           <Route path="/*" element={<Signin />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/send" element={<SendMoney />} />
-          <Route path="/user-profile" element={<UserProfile />} />
+          <Route
+            path="/dashboard"
+            element={<ProtectedRoute element={Dashboard} />}
+          />
+          <Route
+            path="/send"
+            element={<ProtectedRoute element={SendMoney} />}
+          />
+          <Route
+            path="/user-profile"
+            element={<ProtectedRoute element={UserProfile} />}
+          />
         </Routes>
       </BrowserRouter>
     </>
